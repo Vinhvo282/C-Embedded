37cb07d1bd42ce5b3fe07c29d1a515e2a4356154
Vo Quang Vinh <vinh.vo28022005@gmail.com>
2025-11-02 14:28:03 +0700

    Extract a Bit Field from a 32-bit Register

---
+++ Bit Field Operation/extract_BitField.c
@@ -0,0 +1,26 @@
+/*
+You are working with a 32-bit hardware status register. Extract a few bits from it, starting from a given bit position and covering a given length. Return the extracted value as an unsigned integer.
+
+Use 0-based indexing (LSB = position 0).
+
+Example 1
+    Input: reg = 0b1011 0110 0111 0000 0000 0000 0000 0000, pos = 28, len = 4 
+    Output: 0b1011
+Example 2
+    Input: reg = 0b0000 0000 0000 0000 0000 0000 1111 1111, pos = 0, len = 8  
+    Output: 0b11111111
+*/
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t extract_field(uint32_t reg, uint8_t pos, uint8_t len) {
+    return (reg >> pos) & ((1 << len) - 1);
+}
+
+int main() {
+    uint32_t reg;
+    uint8_t pos, len;
+    scanf("%u %hhu %hhu", &reg, &pos, &len);
+    printf("%u", extract_field(reg, pos, len));
+    return 0;
+}
